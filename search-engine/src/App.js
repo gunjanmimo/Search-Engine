c1d6826481e0e8814a8b9b49a53b1345aa8863be
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-08-21 14:38:26 -0700

    new

--- search-engine/src/App.js
+++ search-engine/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import Home from './Home';
 function App() {
   return (
-    <div className="App">
+    // BEM
+    <div className="app">
       <h1>My search Engine</h1>
+
+
+      {/* Home Page (this is with the search bar)*/}
+<Home />
+
+      {/*Search page ( to show all the search result)*/}
     </div>
   );
 }
---
+++ search-engine/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+
+
+export default function Home() {
+    return (
+         <div className = 'home' >
+<h1>This is the homepage</h1>
+        </div>
+    )
+}
