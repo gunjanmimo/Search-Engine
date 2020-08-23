09f120a47c95da92b63897f05fa1fa5235fa613e
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-08-23 15:28:26 -0700

    new

--- search-engine/src/StateProvider.js
+++ search-engine/src/StateProvider.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useReducer } from "react";
 
+//preparing data layer
 export const StateContext = createContext();
 
 export const StateProvider = ({ reducer, initialState, children }) => (
