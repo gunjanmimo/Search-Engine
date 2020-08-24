b531587c4111e969410463f4f1a721a7cfcf197a
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-08-23 20:02:41 -0700

    new

--- search-engine/src/reducer.js
+++ search-engine/src/reducer.js
@@ -1,23 +1,22 @@
-export const initialState={
-    term = null,
+export const initialState = {
+  term: null,
 };
-export const actionTypes={
-    SET_SEARCH_TERM: 'SET_SEARCH_TERM',
+export const actionTypes = {
+  SET_SEARCH_TERM: "SET_SEARCH_TERM",
 };
 
+const reducer = (state, action) => {
+  console.log(action);
 
-const reducer = (state, action)=>{
-    console.log(action);
-
-    switch(action.type){
-        case actionTypes.SET_SEARCH_TERM:
-            return {
-                ... state,
-                term: action.term,
-            };
-            default: 
-                return state;
-    }
+  switch (action.type) {
+    case actionTypes.SET_SEARCH_TERM:
+      return {
+        ...state,
+        term: action.term,
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
