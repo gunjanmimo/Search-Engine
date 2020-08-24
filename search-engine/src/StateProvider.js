a1c88dc104467af49d2865f010e28cfd9b570eb7
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-08-23 19:07:17 -0700

    new

--- search-engine/src/StateProvider.js
+++ search-engine/src/StateProvider.js
@@ -9,4 +9,5 @@ export const StateProvider = ({ reducer, initialState, children }) => (
   </StateContext.Provider>
 );
 
+//Hook which allows us to pull information from the data layer
 export const useStateValue = () => useContext(StateContext);
